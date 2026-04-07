4894d3ae823f291c54e3fec64c5c2c4edfcfe26c
Erik Huemer <58554544+erik-wien@users.noreply.github.com>
2026-04-07 12:16:15 +0200

    feat: update html_header.php — remove Bootstrap/FA/GF, load custom CSS + SVG sprite

--- include/html_header.php
+++ include/html_header.php
@@ -5,33 +5,24 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
   <meta name="application-name" content="WL Monitor">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-title" content="WL Monitor">
   <meta name="theme-color" content="#000000">
-
   <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
   <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png">
   <link rel="manifest" href="img/manifest.json">
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
-        crossorigin="anonymous">
-
-  <!-- Google Fonts -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono|Share+Tech+Mono">
-
-  <!-- Bootstrap 5 -->
-  <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-        crossorigin="anonymous">
-
-  <!-- App styles -->
-  <link rel="stylesheet" href="css/theme.css">
-  <link rel="stylesheet" href="style/wl-monitor.css">
+  <link rel="stylesheet" href="css/base/theme.css">
+  <link rel="stylesheet" href="css/base/reset.css">
+  <link rel="stylesheet" href="css/base/layout.css">
+  <link rel="stylesheet" href="css/base/components.css">
+  <link rel="stylesheet" href="css/app/wl-monitor.css">
 </head>
 <body>
+<?php
+// Inline SVG sprite (one request, cached by browser)
+$_spritePath = __DIR__ . '/../web/css/icons.svg';
+if (file_exists($_spritePath)) { readfile($_spritePath); }
+unset($_spritePath);
+?>
